fe95073f1c8eb7b178e4c4bad5ab0e8bbd7b16f1
harshr3107 <142046984+harshr3107@users.noreply.github.com>
2024-06-22 11:42:43 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (80.66%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,7 +2,44 @@ class Solution {
 public:
     int mySqrt(int x) {
         
-        long long a=1;
+        if(x==1)
+        {
+            return 1;
+        }
+  
+        int low=1;
+        int high=x-1;
+        long long mid=0;
+        long long ans=0;
+        
+        while(high>=low)
+        {
+            mid = low+(high-low)/2;
+            
+           if(mid*mid<=x)
+           {
+               ans=mid;
+               
+               low=mid+1;
+               
+           }else{
+                
+               high=mid-1;
+           }
+            
+            
+            
+            
+        }
+        
+        return ans;
+        
+        
+        
+        
+       /*BRUTE FORCE APPROCH 
+       
+       long long a=1;
         
         while(a*a < pow(2,31)-1 && a*a<=x)
         {
@@ -10,6 +47,7 @@ public:
         }
         
         return a-1;
+        */
         
           
         
