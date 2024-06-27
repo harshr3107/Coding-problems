d245c9cf177db09a82b7458255d70bd8ddec0c93
harshr3107 <142046984+harshr3107@users.noreply.github.com>
2024-06-27 10:24:56 +0530

    Time: 10 ms (95.08%), Space: 26.9 MB (58.86%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         
-        if(s.size()==1)
-        {
-            return;
-        }
+        
         
         int h=s.size();
         
