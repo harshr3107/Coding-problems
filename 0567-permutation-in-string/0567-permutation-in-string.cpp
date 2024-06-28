1734a0563ef103724ad2c87f37525ee6ef449588
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-06-28 11:24:56 +0530

    Time: 55 ms (18.85%), Space: 46.6 MB (12.58%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -7,47 +7,60 @@ public:
         int s2len = s2.length();
         int s1len = s1.length();
         
-        /*for(int i=0;i<s1len;i++)
+        for(int i=0;i<s1len;i++)
         {
             num=num+(s1.at(i)-'a');
         }
         
-        cout<<"value of num is "<<num<<endl;*/
+        //cout<<"value of num is "<<num<<endl;
         
         int i=0;
-        sort(s1.begin(),s1.end());
+        //sort(s1.begin(),s1.end());
         
         while(i<(s2len-s1len+1))
         {
-            string h = s2.substr(i,s1len);
             //cout<<"value of str is "<<h<<endl;
             //sort(s1.begin(),s1.end());
-            sort(h.begin(),h.end());
+            //sort(h.begin(),h.end());
             
-            if(s1==h)
+            /*if(s1==h)
             {
                 return true;
-            }
+            }*/
             
-            i++;
-            /*int j=0;
+            //i++;
+            
+                
+           string h = s2.substr(i,s1len);  
+                
+            int j=0;
             while(j<s1len)
             {
                 cnum+=h.at(j)-'a';
                 j++;
             }
             
-            cout<<"value of cnum is "<<cnum<<endl;
+           // cout<<"value of cnum is "<<cnum<<endl;
             
             if(cnum==num)
             {
+                int j=0;
+                while(j<s1.length() && h.find(s1.at(j))<s1.length())
+                {
+                    j++;
+                }
                 
+                //cout<<"value of j is "<<j<<endl;
+                
+                if(j==s1len)
+                {
                 return true;
+                }
             }
             
             cnum=0;
             i++;
-            */
+            
         }
         
         
