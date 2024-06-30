f3baf3742fa0948c4293fac71821b3905fa212bb
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-06-30 20:13:32 +0530

    Time: 8 ms (21.89%), Space: 12 MB (49.13%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,11 +4,66 @@ public:
         
         int highrow=matrix.size()-1;
         int lowrow=0;
+        int midrow=0;
         
         
         while(highrow>=lowrow)
         {
-            if(matrix[highrow][0]>target)
+            midrow=lowrow+(highrow-lowrow)/2;
+            
+            if(matrix[midrow][0]>target)
+            {
+                highrow=midrow-1;
+                
+            }else{
+                
+                lowrow=midrow+1;
+                
+            } 
+            
+            
+        }
+        
+        cout<<"high row is "<<highrow;
+        
+        if(highrow==-1)
+        {
+            return false;
+        }
+        
+       
+        int low=0;
+        int high=matrix[0].size()-1;
+        int mid;
+        
+       while(high>=low)
+        {
+            mid=low+(high-low)/2;
+            
+            if(matrix[highrow][mid]==target)
+            {
+                return true;
+            }else if(matrix[highrow][mid]>target)
+            {
+                high=mid-1;
+            }else{
+                
+                low=mid+1;
+            }
+        }
+        
+        
+        
+        
+        
+        return false;
+        
+    }
+};
+
+
+
+/*if(matrix[highrow][0]>target)
             {
                 highrow--;
             }else{
@@ -35,20 +90,4 @@ public:
                 
                 break;
                 
-            }
-            
-            
-            
-        }
-        
-       
-        
-        
-        
-        
-        
-        
-        return false;
-        
-    }
-};
+            }*/
