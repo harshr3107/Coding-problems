978181e1482fa7e6b53d5df784b11f9b0556700c
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-07-05 11:32:18 +0530

    Time: 0 ms (100.00%), Space: 16.6 MB (34.21%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -2,6 +2,66 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         
+        
+        int low=0;
+        int high=nums.size()-1;
+        int mid=0;
+        
+        
+        while(high>=low)
+        {
+            mid = low+(high-low)/2;
+            
+            if(nums[mid]==target)
+            {
+                return true;
+            }
+            if(nums[mid]==nums[high] && nums[mid]==nums[low])
+            {
+                low++;
+                high--;
+                continue;
+            }
+            
+            if(nums[low]<=nums[mid])
+            {
+                
+                if(nums[low]<=target && nums[mid]>=target)
+                {
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+                
+                
+                
+            }else{   
+            
+           
+                
+                if(nums[mid]<=target && nums[high]>=target)
+                {
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+                
+                
+            
+                
+       }
+            
+        }
+            
+            
+        return false;
+        
+        
+        
+        
+        
+        
+        
        /* int low=0;
         int high=nums.size();
         int mid;
@@ -87,7 +147,7 @@ public:
         */
         
         
-        int i=0;
+      /*  int i=0;
         while(i<nums.size()-1 && nums[i+1]>=nums[i])
         {
             i++;
@@ -136,6 +196,12 @@ public:
         }
         
         return false;
+        */
+        
+        
+        
+        
+        
         
         
         
