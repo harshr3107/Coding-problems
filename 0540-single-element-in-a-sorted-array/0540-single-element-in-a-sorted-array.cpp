5e4eb65fcc4a0d1584c99b9a88553420d37438b4
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-07-06 19:29:48 +0530

    Time: 21 ms (40.95%), Space: 24.6 MB (51.59%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+     
+        int low=0;
+        int high=nums.size()-1;
+        int mid=0;
+        
+        
+        if( nums.size()==1)
+        {
+            return nums[0];
+        }
+        
+        while(high>=low)
+        {
+            mid = low+(high-low)/2;
+            
+            if((mid&1))
+            {
+                //odd number
+                if(nums[mid]!=nums[mid-1])
+                {
+                    if(mid<nums.size()-1 && nums[mid]!=nums[mid+1])
+                    {
+                        return nums[mid];
+                    }
+                    
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+                
+            }else{
+                
+                //even number
+                if(mid<nums.size()-1 && nums[mid]!=nums[mid+1])
+                {
+                    if(mid>0 && nums[mid]!=nums[mid-1])
+                    {
+                        return nums[mid];
+                    }
+                    high=mid-1;
+                    
+                }else{
+                    
+                    low=mid+1;
+                    
+                }
+                
+                
+            }
+            
+        }
+        
+        
+        return nums[mid];
+        
+        
+        
+        
+        
+    }
+};
