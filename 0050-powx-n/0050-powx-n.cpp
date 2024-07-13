2aaac4a117d396787a43c1723f119e26ec905c77
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-07-13 13:19:03 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (45.31%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     
     
@@ -29,5 +29,24 @@ public:
         
        // return 0;
         
+    }
+};*/
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        if(n==0){
+            return 1;
+        }
+        if (n < 0) { 
+            n = abs(n);
+            x = 1/x;
+        }
+        if(n%2==0){
+            return myPow(x*x,n/2);
+        
+        }else{
+            return x*myPow(x*x,n/2);
+        }
     }
 };
