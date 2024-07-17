48bd8ce6f9c6f2362ea49a6cd146da3d9b5590a1
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-07-17 12:35:46 +0530

    Time: 11 ms (14.63%), Space: 17.4 MB (12.32%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        vector<vector<int>> ans;
+        
+        vector<int> v;
+       
+        sort(candidates.begin(),candidates.end());
+        getcombi(candidates,0,target,v,ans);
+        
+        
+        
+        
+        
+        return ans;
+        
+        
+    }
+    
+    
+    
+    void getcombi(vector<int> candidates,int ind,int target,vector<int> v,vector<vector<int>>& ans)
+    {
+        
+       
+        
+      
+        
+            if(target==0)
+            {
+            
+              ans.push_back(v);
+                return;
+            }
+            
+            
+                
+    
+        
+        
+        for(int i=ind;i<candidates.size();i++)
+        {
+            if(i>ind && candidates[i]==candidates[i-1])
+            {
+                continue;
+            }
+            if(candidates[i]>target)
+            {
+                break;
+            }
+        
+       
+        v.push_back(candidates[i]);
+        getcombi(candidates,i+1,target-candidates[i],v,ans);
+        v.pop_back();
+        
+        }
+        
+            
+        
+        
+    }
+    
+    
+    
+    
+    
+};
