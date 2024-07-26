848bb5dbb8631ce2bea4cd654fe6118a821fee8c
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-07-26 22:29:34 +0530

    Time: 43 ms (73.04%), Space: 17.3 MB (89.73%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+
+        int low=0;
+        int high=0;
+        int turn=0;
+
+
+         int vowels=0;
+         for(int i=0;i<s.length();i++)
+         {
+
+            if(s.at(i)=='a' || s.at(i)=='i' || s.at(i)=='e' || s.at(i)=='o' || s.at(i)=='u')
+            {
+                  vowels++;
+            }
+
+
+         }
+
+         cout<<"number of vowels are "<<vowels<<endl;
+
+
+
+        if(vowels==0)
+        {
+            return false;
+        }
+
+        return true;
+
+    }
+
+
+       
+};
