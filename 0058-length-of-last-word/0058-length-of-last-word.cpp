6242b502a9146a7543b1f0d3ff53c890db570f40
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-07-28 16:48:39 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (7.82%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int i=s.length()-1;
+        
+        
+          while(i>=0 && s.at(i)==' ')
+          {
+              i--;
+          }
+        
+       int  a=0;
+        
+        while(i>=0 && s.at(i)!=' ')
+        {
+            a++;
+            i--;
+        }
+        
+        
+        return a;
+        
+        
+        
+            
+        
+    }
+};
