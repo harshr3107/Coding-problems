f6311bf31363704db5e4c0eaf61b33541ca4d59a
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-07-30 07:58:20 +0530

    Time: 29 ms (63.38%), Space: 13.2 MB (57.90%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -3,9 +3,61 @@ public:
     int firstUniqChar(string s) {
         
         vector<int> count(26);
-        //queue<int> q;
+        queue<int> q;
         
         
+        
+        int i=0;
+       
+        
+        while(i<s.length())
+        {
+            //cout<<"value of q front is "<<q.front()<<endl;
+            
+            while(!q.empty() && count[s.at(q.front())-'a']>1)
+            {
+                //cout<<"i entered here for "<<i<<endl;
+                q.pop();
+            }
+           
+            
+            //cout<<"frequency of i is "<<count[s.at(i)-'a']<<endl;
+            if(count[s.at(i)-'a']==0)
+            {
+               q.push(i);
+            }
+            count[s.at(i)-'a']++;
+            i++;
+            
+        }
+        
+        
+        if(!q.empty() && count[s.at(q.front())-'a']>1)
+        {
+            q.pop();
+        }
+        
+        if(q.empty())
+        {
+            return -1;
+        }
+        
+        return q.front();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       //bRUTE FORCE APPROACH
+        /*
         int i=0;
         
         
