5ed2c133df16a249e6c9df3c3f8cc71ea0b8f0d7
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-08-06 09:02:07 +0530

    Time: 4 ms (33.96%), Space: 8.2 MB (30.19%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -4,56 +4,46 @@ public:
         
         
         
-        map<int,int> m1;
-        map<char,int> m2;
+       
+       
         int a=0;
         int push=0;
         
         for(int i=0;i<word.size();i++)
         {
-            m1[word.at(i)]++;
+           
             
-            if(m1[word.at(i)]==1)
-            {
+            
                 a++;
                 if(a<=8)
                 {
-                    m2[word.at(i)]=1;
+                   
                     push++;
                     
                     
-                }else{
-                    //cout<<"mai yah aaya hoon bhai\n";
-                   
-                    
-                    if(a<=16)
-                    {
-                        m2[word.at(i)]=2;
+                }else if(a<=16)
+                {
+                      
                         push+=2;
                         
-                    }else if(a<=24)
-                    {
-                        m2[word.at(i)]=3;
-                        push+=3;
-                    }else{
+                }else if(a<=24)
+                {
                         
-                        m2[word.at(i)]=4;
+                        push+=3;
+                }else{
+                        
+                       
                         push+=4;
-                    }
-                    
-                    
-                    
-                    
                 }
+                    
+                    
+                    
+                    
+       }
                 
-            }else{
-                
-                
-                push+=m2[word.at(i)];
-                
-            }
             
-        }
+            
+        
         
         
         
