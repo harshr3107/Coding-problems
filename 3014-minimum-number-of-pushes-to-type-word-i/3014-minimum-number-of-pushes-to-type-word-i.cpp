1a4bdcd1176fcb21c0e5c5df41eeab8c3f0f1bb4
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-08-06 08:58:30 +0530

    Time: 6 ms (9.12%), Space: 11.6 MB (5.45%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        
+        
+        sort(word.begin(),word.end());
+        
+        map<int,int> m1;
+        map<char,int> m2;
+        int a=0;
+        int push=0;
+        
+        for(int i=0;i<word.size();i++)
+        {
+            m1[word.at(i)]++;
+            
+            if(m1[word.at(i)]==1)
+            {
+                a++;
+                if(a<=8)
+                {
+                    m2[word.at(i)]=1;
+                    push++;
+                    
+                    
+                }else{
+                    //cout<<"mai yah aaya hoon bhai\n";
+                   
+                    
+                    if(a<=16)
+                    {
+                        m2[word.at(i)]=2;
+                        push+=2;
+                        
+                    }else if(a<=24)
+                    {
+                        m2[word.at(i)]=3;
+                        push+=3;
+                    }else{
+                        
+                        m2[word.at(i)]=4;
+                        push+=4;
+                    }
+                    
+                    
+                    
+                    
+                }
+                
+            }else{
+                
+                
+                push+=m2[word.at(i)];
+                
+            }
+            
+        }
+        
+        
+        
+        return push;
+        
+        
+    }
+};
