1605f389384f62f515cf8ae84f52be2cf0b76989
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-08-10 20:06:38 +0530

    Time: 102 ms (5.02%), Space: 48.1 MB (5.05%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        
+         map<int,int> mp;
+        mp[0]=0;
+        int sum=0;
+        int h=0;
+        int size=INT_MAX;
+        map<int,int>:: iterator it;
+        int b=0;
+        int cursize=0;
+        
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>=target)
+            {
+                return 1;
+            }
+            
+            
+            sum=sum+nums[i];
+            mp[sum]=i;
+            
+            if(sum>=target)
+            {
+               //cout<<" i entered here-1\n"; 
+                b++;
+               size = min(size,i+1);
+            }
+            h = sum-target;
+            
+            if(h<0)
+            {
+               // cout<<"i jjj\n";
+                continue;
+            }
+           
+            it = mp.lower_bound(h);
+            
+            while(it!=mp.begin())
+            {
+                //cout<<"i entered here\n "<<it->first<<" "<<h<<endl;
+                
+                if((it->first)<=h)
+                {
+                    //cout<<"yaha pe aa n\n";
+                    
+                    b++;
+                    cursize = i-(it->second);
+                    size = min(size,cursize);
+                    //cout<<"yes i am there at "<<i<<" "<<it->second<<endl;
+                    break;
+                }
+                
+                it--;
+            }
+            
+            
+           
+            
+            
+        }
+        
+        
+        
+        if(b==0)
+        {
+            return 0;
+        }
+        
+        
+        return size;
+        
+        
+    }
+};
