8ff67052eff74e3c3c5965159f39158be09b5f30
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-09-07 21:32:24 +0530

    Time: 205 ms (26.54%), Space: 72.7 MB (11.51%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    bool isPossibleDivide(vector<int>& nums, int k) {
-        
-        sort(nums.begin(),nums.end());
-        
-        
-        while(!nums.empty())
-        {
-            int i=0;
-            int a=1;
-            int n =nums[0];
-            
-            while(a<=k && find(nums.begin(),nums.end(),n)!=nums.end())
-            {
-                //cout<<"i found the num "<<n<<endl;
-                a++;
-                nums.erase(find(nums.begin(),nums.end(),n));
-                n=n+1;
-               
-            }
-            
-            //cout<<endl;
-            //cout<<"value of a is "<<a<<" value of k is "<<k<<endl<<endl;
-            a=a-1;
-            
-            if(a<k)
-            {
-                return false;
-            }
-            
+    bool isPossibleDivide(vector<int>& hand, int groupSize) {
+        if(hand.size() % groupSize != 0)return false;
+        map<int,int> umap;
+        for(int i=0; i<hand.size(); i++){
+            umap[hand[i]]++;
         }
-        
-        
+        int start = 0;
+        int curr;
+        while(umap.size()>0){
+            curr = umap.begin()->first;
+            for(int i=0; i<groupSize; i++){
+                if(umap[curr+i] == 0)return false;
+                --umap[curr+i];
+                if(umap[curr+i] == 0)umap.erase(curr+i);
+            }
+        }
         return true;
-        
     }
 };
