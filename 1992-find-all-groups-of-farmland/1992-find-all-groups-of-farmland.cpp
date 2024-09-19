62b9d85ed0708261e90b4c9b626c89b9d3ebc001
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-09-19 15:45:22 +0530

    Time: 276 ms (12.79%), Space: 152.1 MB (6.07%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -3,108 +3,72 @@ public:
     
     
     
-    void getbfs(vector<vector<int>>& land,int cr,int cc,vector<vector<bool>>& visited,vector<int>& v)
+    void dfs(vector<vector<char>>& grid,int cr,int cc,vector<vector<bool>>& visited)
     {
         
         visited[cr][cc]=true;
         
-        int a=cr;
-        int b=cc;
         
-        queue<pair<int,int>> q;
-        q.push(make_pair(cr,cc));
-        
-        
-        while(!q.empty())
+        if((cr-1)>=0 && grid[cr-1][cc]=='1' && visited[cr-1][cc]==false)
         {
             
-            pair<int,int> temp = q.front();
-            q.pop();
-            int row = temp.first;
-            int col = temp.second;
-            a=row;
-            b=col;
-            
-            
-            if((row-1)>=0 && land[row-1][col]==1 && visited[row-1][col]==false)
-            {
-                visited[row-1][col]=true;
-                q.push(make_pair(row-1,col));
-                
-            }
-            
-            if((row+1)<land.size() && land[row+1][col]==1 && visited[row+1][col]==false)
-            {
-                visited[row+1][col]=true;
-                q.push(make_pair(row+1,col));
-                
-            }
-            
-            if((col-1)>=0 && land[row][col-1]==1 && visited[row][col-1]==false)
-            {
-                visited[row][col-1]=true;
-                q.push(make_pair(row,col-1));
-                
-            }
-            
-            if((col+1)<land[row].size() && land[row][col+1]==1 && visited[row][col+1]==false)
-            {
-                visited[row][col+1]=true;
-                q.push(make_pair(row,col+1));
-                
-            }
-            
-            
-            
-            
-            
-            
+            dfs(grid,cr-1,cc,visited);
         }
         
-        cout<<"val of a and b here is "<<a<<" "<<b<<endl;
         
-     
-        v.push_back(a);
-        v.push_back(b);        
+        
+        
+        if((cr+1)<grid.size() && grid[cr+1][cc]=='1' && visited[cr+1][cc]==false)
+        {
+            
+            dfs(grid,cr+1,cc,visited);
+        }
+        
+        
+        
+        
+        if((cc-1)>=0 && grid[cr][cc-1]=='1' && visited[cr][cc-1]==false)
+        {
+            
+            dfs(grid,cr,cc-1,visited);
+        }
+        
+         if((cc+1)<grid[cr].size() && grid[cr][cc+1]=='1' && visited[cr][cc+1]==false)
+        {
+            
+            dfs(grid,cr,cc+1,visited);
+        }
+        
         
     }
     
     
     
     
-    
-    
-    
-    
-    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+    int numIslands(vector<vector<char>>& grid) {
         
         
         vector<vector<bool>> visited;
         
-        for(int i=0;i<land.size();i++)
+        for(int i=0;i<grid.size();i++)
         {
-           vector<bool> v(land[i].size(),false);
-           visited.push_back(v); 
-            
+            vector<bool> v(grid[i].size(),false);
+            visited.push_back(v);
         }
         
+        int ans=0;
         
-        vector<vector<int>> ans;
-        
-        for(int i=0;i<land.size();i++)
+        for(int i=0;i<grid.size();i++)
         {
-            for(int j=0;j<land[i].size();j++)
+            
+            for(int j=0;j<grid[i].size();j++)
             {
-                if(land[i][j]==1 && visited[i][j]==false)
+                if(grid[i][j]=='1' && visited[i][j]==false)
                 {
-                    vector<int> v;
-                    v.push_back(i);
-                    v.push_back(j);
-                    getbfs(land,i,j,visited,v);
-                    ans.push_back(v);
+                    ans++;
+                    dfs(grid,i,j,visited);
                     
                 }
-                
             }
             
         }
@@ -112,6 +76,8 @@ public:
         
         
         return ans;
+        
+        
         
         
         
