b1833ef53e79ca47f701ce02bf9eb1201304174f
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-09-25 16:11:03 +0530

    Time: 12 ms (44.06%), Space: 16.4 MB (83.98%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,30 +10,117 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head, ListNode* tail) {
-        ListNode* current = head;
-        ListNode *prev = NULL, *next = NULL;
- 
-        while (current != tail) {
-            next = current->next;
-            current->next = prev;
-            prev = current;
-            current = next;
-        }
+    
+    
+    ListNode* reverse(ListNode* head)
+    {
+        ListNode* prev=NULL;
+        ListNode* curr = head;
+        ListNode* nextt;
+        
+        while(curr!=NULL)
+        {
+            nextt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextt;
+        }    
+        
         
         return prev;
+            
+        
+        
     }
     
+    
+    
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* ptr = head;
         
-        for (int i = 0; i < k; i++) {
-            if (!ptr) return head;
-            ptr = ptr->next;
+        int a=0;
+        ListNode* dummy=new ListNode(-1);
+        dummy->next=head;
+        
+        ListNode* prev=dummy;
+        
+        ListNode* temp=dummy;
+        
+        
+        while(temp!=NULL)
+        {
+            cout<<"the temp ka data is "<<temp->val<<endl;
+            
+            a=0;
+            while(temp!=NULL && a!=k)
+            {
+                temp=temp->next;
+                a++;
+            }
+            
+            ListNode* nextt;
+            
+            if(temp!=NULL)
+            {
+                
+                
+                nextt=temp->next;
+                temp->next=NULL;
+                
+            }else{
+                
+                cout<<"i entered here\n";
+                cout<<"val of a here is "<<a<<endl;
+                
+                if(a!=k+1)
+                {
+                    break;
+                }
+                
+                 ListNode* newhead = reverse(prev->next);
+             //   cout<<"yaha pe new head will be "<<newhead->val<<endl;
+                
+                prev->next=newhead;
+                break;
+                   
+                nextt=NULL;
+                temp=NULL;
+            }
+            
+           
+            ListNode* newhead = reverse(prev->next);
+            
+            if(newhead==NULL)
+            {
+                break;
+            }
+            
+            cout<<"\nthe new heads is  "<<newhead->val<<endl<<endl;
+            
+            prev->next=newhead;
+            
+            if(prev->val==dummy->val)
+            {
+                dummy->next=newhead;
+            }
+            
+            while(prev->next!=NULL)
+            {
+                prev=prev->next;
+            }
+            
+            prev->next=nextt;
+           
+            temp=prev;
+            
+            
             
         }
-        ListNode* tmp = reverse(head, ptr);
-        head->next = reverseKGroup(ptr, k);
-        return tmp;
+        
+        
+        
+        return dummy->next;
+        
+        
+        
     }
 };
