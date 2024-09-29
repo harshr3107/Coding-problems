abf06fbc76f438a5de9f06936e65231624b6abb7
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-09-29 14:16:36 +0530

    Time: 16 ms (8.55%), Space: 18.7 MB (33.65%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,103 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        
+        if(root==NULL)
+        {
+            return root;
+        }
+        
+        
+        queue<Node*> q;
+        q.push(root);
+        q.push(NULL);
+        
+        vector<Node*> v;
+        
+        while(!q.empty())
+        {
+            
+            Node* temp = q.front();
+            q.pop();
+            
+            
+            if(temp==NULL)
+            {
+                
+                
+                
+                if(v.size()==1)
+                {
+                    v[0]->next=NULL;
+                }else{
+                    
+                    int i;
+                    for(i=0;i<v.size()-1;i++)
+                    {
+                        v[i]->next=v[i+1];
+                        v[i+1]->next=NULL;
+                    }
+                    
+                    
+                }
+                
+                
+                
+                
+                v.clear();
+                
+                
+                if(!q.empty())
+                {
+                    q.push(NULL);
+                }
+                
+                
+            }else{
+                
+                v.push_back(temp);
+                
+                if(temp->left!=NULL)
+                {
+                    q.push(temp->left);
+                    
+                }
+                
+                if(temp->right!=NULL)
+                {
+                    q.push(temp->right);
+                }
+                
+                
+                
+            }
+            
+            
+            
+            
+            
+        }
+        
+        
+        
+        return root;
+    }
+};
