61abe44e59dde0d289f3c96c8f2af5ae3459e3d6
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-10-15 18:39:12 +0530

    Time: 256 ms (53.45%), Space: 93.5 MB (88.36%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,62 +1,25 @@
 class Solution {
 public:
-    
-    
-    bool static comp(vector<int>& a,vector<int>& b)
-    {
-        if(a[0]==b[0])
-        {
-            return a[1]>b[1];
-        }
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if (points.empty()) return 0;
+
+        // Sort intervals by their end times
+        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];  // Sort by end time
+        });
+
+        int arrows = 1;  // Start with one arrow
+        int lastEnd = points[0][1];  // Set the end time of the first balloon
         
-        return a[0]<b[0];
-            
-    }
-  
-    
-    
-    
-   
-    
-    int findMinArrowShots(vector<vector<int>>& intervals) {
-        
-        sort(intervals.begin(),intervals.end(),comp);
-        
-         pair<int,int> mini;
-         int ans=0;
-        
-        
-        
-        
-        for(int i=0;i<intervals.size();i++)
-        {
-           // cout<<intervals[i][0]<<" "<<intervals[i][1]<<endl;
-            //cout<<"val of ans is "<<ans<<endl;
-            
-            if(i!=0 && ((mini.first<=intervals[i][0] && mini.second>=intervals[i][0])) )
-            {
-                mini.first = max(mini.first,intervals[i][0]);
-                mini.second = min(mini.second,intervals[i][1]);
-                
-               continue;
+        // Loop through the intervals
+        for (int i = 1; i < points.size(); ++i) {
+            // If the current balloon starts after the last one ended, we need a new arrow
+            if (points[i][0] > lastEnd) {
+                arrows++;  // Shoot a new arrow
+                lastEnd = points[i][1];  // Update the end time to the current balloon's end
             }
-    
-            
-            //min=intervals[i][1];
-            mini.first=intervals[i][0];
-            mini.second=intervals[i][1];
-            ans++;
-            
         }
-        
-       
-        
-       
-            
-        
-        
-        
-        return ans;
-        
+
+        return arrows;  // Return the total number of arrows needed
     }
 };
