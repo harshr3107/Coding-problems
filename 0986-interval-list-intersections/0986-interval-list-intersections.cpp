b99e2a1fd1d919322d7cac9f444893255e7315f9
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-10-16 11:10:59 +0530

    Time: 41 ms (5.26%), Space: 23 MB (24.42%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& f, vector<vector<int>>& s) {
+        
+        
+        int i=0;
+        int j=0;
+        
+        vector<vector<int>> ans;
+         vector<int> v;
+        
+        while(i<f.size() && j<s.size())
+        {
+            
+            if(f[i][0]<=s[j][0])
+            {
+                cout<<"i entered here 1\n";
+                
+                if(f[i][1]>=s[j][0])
+                {
+                   
+                    
+                    int a = s[j][0];
+                    int b = min(f[i][1],s[j][1]);
+                    
+                    v.push_back(a);
+                    v.push_back(b);
+                    ans.push_back(v);
+                    v.clear();
+                }
+                
+                
+                
+            }else{
+                
+                cout<<"i entered here 2\n";
+                
+                if(s[j][1]>=f[i][0])
+                {
+                    vector<int> v;
+                    
+                    int a = f[i][0];
+                    int b = min(f[i][1],s[j][1]);
+                    
+                    v.push_back(a);
+                    v.push_back(b);
+                    
+                    ans.push_back(v);
+                    v.clear();
+                }
+                
+                
+                
+            }
+            
+            
+            
+            
+            
+            
+            if(f[i][1]>s[j][1])
+            {
+                j++;
+            }else{
+                i++;
+            }
+            
+        }
+        
+        
+        return ans;
+        
+        
+        
+    }
+};
