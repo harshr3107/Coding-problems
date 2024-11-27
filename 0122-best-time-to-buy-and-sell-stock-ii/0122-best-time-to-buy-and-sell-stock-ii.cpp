69d8506f09ddfa89ca48d998b24d40c743c6ad96
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-11-27 18:20:28 +0530

    Time: 7 ms (15.13%), Space: 19.7 MB (15.56%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -55,46 +55,60 @@ public:
     
     int maxProfit(vector<int>& prices) {
         
-        vector<vector<int>> dp(prices.size()+1,vector<int>(2,-1));
+        /*vector<vector<int>> dp(prices.size()+1,vector<int>(2,-1));
         
         int index=0;
         int buy=0;
         
-        return getmax(prices,0,true,dp);
+        return getmax(prices,0,true,dp);*/
+        
+        vector<vector<int>> dp(prices.size()+1,vector<int>(2,0));
+        
+        dp[prices.size()][1]=dp[prices.size()][0]=0;
+        
+        for(int index=prices.size()-1;index>=0;index--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                
+                 int profit=0;
+        if(buy)
+        {
+            //buy karle
+            int price1 = -prices[index]+dp[index+1][0];
+            //ignore karle
+            int price2 = dp[index+1][1];
+            
+            profit = max(price1,price2);
+            
+            
+        }else{
+            
+            //sell karle
+            
+            int price1 = prices[index]+dp[index+1][1];
+            
+            //ignore karle
+            
+            int price2 =dp[index+1][0];
+            
+            profit = max(price1,price2);
+            
+        }
+        
+        dp[index][buy]=profit;
+        
+                
+                
+                
+            }
+            
+        }
+        
+        
+     return dp[0][1];   
         
     }
 };
-/*
-class Solution {
-private:
-    
-    int solveMem(vector<int>& prices,int index,int buy,vector<vector<int>> &dp)
-    {
-             if(index==prices.size()) return 0;
-             int profit=0;
 
-             if(dp[index][buy]!=-1) return dp[index][buy];
 
-             if(buy)
-             {
-                int buykaro=-prices[index]+solveMem(prices,index+1,0,dp);
-                int skipkaro=0+solveMem(prices,index+1,1,dp);
-                 profit=max(buykaro,skipkaro);
-             }else
-             {
-                int sellkaro=+prices[index]+solveMem(prices,index,1,dp);
-                int ignorekaro=0+solveMem(prices,index+1,0,dp);
-                profit=max(sellkaro,ignorekaro);
-             }
-             return dp[index][buy]=profit;
-    }  
-
- 
-public:
-    int maxProfit(vector<int>& prices) {
-         int n=prices.size();
-         vector<vector<int>> dp(n+1,vector<int>(2,-1));
-         return solveMem(prices,0,1,dp);
-    }
-};
-*/
