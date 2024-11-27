76ec542b332f7b5f21f115ea61f8bb57bd201f24
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-11-27 20:57:47 +0530

    Time: 7 ms (63.35%), Space: 17.7 MB (17.14%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    
+    
+    //is ka sol ke pehle bset time 3 ki video dekh striver ki u will gwt it why i ttok k*2
+    
+    int getmax(vector<int>& prices,int index,int ctrans,int k,vector<vector<int>>& dp)
+    {
+        
+        if(index==prices.size() || ctrans==k*2)
+        {
+            return 0;
+        }
+        
+        if(dp[index][ctrans]!=-1)
+        {
+            return dp[index][ctrans];
+        }
+        
+        int profit=0;
+        
+        if((ctrans&1)==0)
+        {
+           
+            //buy
+            
+            int price1 = -prices[index]+getmax(prices,index+1,ctrans+1,k,dp);
+            int price2 = getmax(prices,index+1,ctrans,k,dp);
+            
+            profit = max(price1,price2);
+            
+        }else{
+            
+            
+            
+            int price1 = prices[index]+getmax(prices,index+1,ctrans+1,k,dp);
+            int price2 = getmax(prices,index+1,ctrans,k,dp);
+            
+            profit = max(price1,price2);
+            
+            
+        }
+        
+        dp[index][ctrans]=profit;
+        return profit;
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    int maxProfit(int k, vector<int>& prices) {
+        
+        int h = k*2;
+        vector<vector<int>> dp(prices.size()+1,vector<int>(h+1,-1));
+        int index=0;
+        int ctrans=0;
+        return getmax(prices,index,ctrans,k,dp);
+        
+        
+    }
+};
