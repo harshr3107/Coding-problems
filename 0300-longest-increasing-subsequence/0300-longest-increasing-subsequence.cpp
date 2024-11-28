3aabb257c0f249ef0a59304386f93450b5a78397
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-11-28 10:43:16 +0530

    Time: 663 ms (5.01%), Space: 296.8 MB (5.05%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     
-    int getmaxseq(vector<int>& nums,int index,int lastind,vector<vector<int>>& dp)
+   /* int getmaxseq(vector<int>& nums,int index,int lastind,vector<vector<int>>& dp)
     {
         
         if(index>=nums.size())
@@ -34,17 +34,60 @@ public:
         return max(take,nottake);
         
         
+    }*/
+    
+    
+    //trting to conver to 1 based indexing
+    
+     int getmaxseq(vector<int>& nums,int index,int lastind,vector<vector<int>>& dp)
+    {
+        
+        if(index<0)
+        {
+            return 0;
+        }
+        
+        if(dp[index][lastind]!=-1)
+        {
+            return dp[index][lastind];
+        }
+        
+        
+        int take=0;
+        
+        if(lastind==nums.size() || nums[lastind]>nums[index])
+        {
+            take = 1+getmaxseq(nums,index-1,index,dp);
+        }
+        
+        int nottake = getmaxseq(nums,index-1,lastind,dp);
+        
+        
+          dp[index][lastind]=max(take,nottake);
+          return dp[index][lastind]; 
+     
+        
     }
     
     
     int lengthOfLIS(vector<int>& nums) {
      
-        int index=0;
+       /* int index=0;
         int lastind = -1;
         int lasttaken=INT_MIN;
         vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+1,-1));
         
         return getmaxseq(nums,index,lastind,dp);
+        */
+        
+         int index=nums.size()-1;
+        int lastind = nums.size();
+        
+        vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        
+        return getmaxseq(nums,index,lastind,dp);
+        
+        
         
      
         
