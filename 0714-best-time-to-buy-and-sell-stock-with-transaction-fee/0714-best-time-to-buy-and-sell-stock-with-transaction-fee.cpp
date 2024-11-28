e21f909ace48b1cb6f7bc4bf1f5ca999dac0edbc
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-11-28 10:10:22 +0530

    Time: 94 ms (42.28%), Space: 95 MB (35.03%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -49,11 +49,56 @@ public:
     int maxProfit(vector<int>& prices, int fee) {
         
         
-      vector<vector<int>> dp(prices.size()+1,vector<int>(2,-1));
+     /* vector<vector<int>> dp(prices.size()+1,vector<int>(2,-1));
         int index=0;
         bool buy=true;
         
-        return getmax(prices,index,buy,fee,dp);
+        return getmax(prices,index,buy,fee,dp);*/
+        
+        
+        //Tabulation
+        
+        vector<vector<int>> dp(prices.size()+1,vector<int>(3,0));
+        
+        int n = prices.size();
+        
+        for(int index=n-1;index>=0;index--)
+        {
+            
+            for(int buy=0;buy<=1;buy++)
+            {
+                
+                 int profit=0;
+        
+        if(buy)
+        {
+            int price1 = -prices[index]+dp[index+1][0];
+            int price2 = dp[index+1][1];
+            
+            profit = max(price1,price2);
+            
+        }else{
+            
+            int price1 = prices[index]+dp[index+1][1];
+            int price2 = dp[index+1][0];
+            
+            profit = max(price1-fee,price2);
+            
+            
+        }
+        
+        dp[index][buy]=profit;
+                
+        }
+            
+            
+        }
+        
+        
+        return dp[0][1];
+        
+        
+        
         
         
     }
