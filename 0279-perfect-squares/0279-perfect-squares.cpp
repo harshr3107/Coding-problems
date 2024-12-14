c56292cc00e5c679280b26039d78b588776c4a07
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-12-14 15:25:53 +0530

    Time: 191 ms (16.65%), Space: 165 MB (5.04%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,94 @@
+class Solution {
+public:
+    
+    
+    int getmin(int i,int n,vector<vector<int>>& dp)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        
+        if(i<=0)
+        {
+            return 1e9+7;
+        }
+        
+        if(dp[i][n]!=-1)
+        {
+            return dp[i][n];
+        }
+        
+        int take = 1e9+7;
+        
+        if(n-pow(i,2)>=0)
+        {
+            take=1+getmin(i,n-pow(i,2),dp);
+        }
+        
+        //cout<<"val of i = "<<i<<" and n = "<<n<<endl;
+        
+        int nottake = getmin(i-1,n,dp);
+        
+        dp[i][n]=min(take,nottake);
+        return dp[i][n];
+        
+        
+    }
+    
+    
+    int numSquares(int n) {
+        
+        
+         int i=ceil(sqrt(n));
+         vector<vector<int>> dp(i+1,vector<int>(n+1,-1));
+        
+        return getmin(i,n,dp);
+        
+        /*
+         vector<vector<int>> dp(i+1,vector<int>(n+1,INT_MAX));
+        
+        for(int k=0;k<=i;k++)
+        {
+            dp[k][0]=0;
+        }
+        
+        //return getmin(i,n,dp);
+        
+        
+         /*for(int i=1;i<=sqrt(n)+1;i++)
+         {
+             for(int j=0;j<=n;j++)
+             {
+                 
+                 
+                 
+                 int take = 1e9+7;
+        
+                 if(j-pow(i,2)>=0)
+                 {
+                    take=1+dp[i][j-pow(i,2)];
+                  }
+        
+        //cout<<"val of i = "<<i<<" and n = "<<n<<endl;
+        
+        int nottake = dp[i-1][j];
+        
+        dp[i][j]=min(take,nottake);
+                 
+                 //cout<<dp[i][j]<<" ";
+                 
+                 
+                 
+             }
+             
+             //cout<<endl;
+         }
+         
+        
+        return dp[i][n];
+         
+       */
+        
+    }
+};
