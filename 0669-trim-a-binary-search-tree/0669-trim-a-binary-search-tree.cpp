84eb5069f524aeb639bdb9a5e9ce1f2f92cd1272
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-12-18 11:47:35 +0530

    Time: 0 ms (100.00%), Space: 21.5 MB (87.90%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
@@ -9,71 +9,44 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/*
 class Solution {
 public:
     
+
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    void getbst(TreeNode*& root,int low,int high)
+   void getnewtree(TreeNode*& root,int low,int high)
     {
-        
-        
         if(root==NULL)
         {
             return;
-        }   
-        
-        if(root->left!=NULL)
-        {
-            if(root->val>high)
-            {
-                root=root->left->left;
-            }else if(root->val<low)
-            {
-                root=root->left->right;
-            }
-            
-            
         }
-        
-        if(root->right!=NULL)
-        {
-            if(root->val>high)
-            {
-                root=root->right->left;
-            }else if(root->val<low)
-            {
-                root=root->right->right;
-            }
-            
-            
-        }
-        
-        
-        
-        getbst(root->left,low,high);
-        getbst(root->right,low,high);
-        
-            
-        
-        
-        
-        
+         
+       
+         if(root->val>=low && root->val<=high)
+         {
+             getnewtree(root->left,low,high);
+             getnewtree(root->right,low,high);
+             
+         }else if(root->val<=low){
+             
+             
+            root->left=NULL;
+            root=root->right;
+            getnewtree(root,low,high);
+             
+             
+             
+         }else{
+             
+             root->right=NULL;
+             root=root->left;
+             getnewtree(root,low,high);
+             
+         }
+       
+       
     }
-    
-    
-    
-    
     
     
     
@@ -81,37 +54,10 @@ public:
     
     TreeNode* trimBST(TreeNode* root, int low, int high) {
         
-        getbst(root,low,high); 
+        getnewtree(root,low,high);
         
         return root;
         
-    }
-};
-*/
-
-
-class Solution {
-public:
-    void Trim(TreeNode* root, int low, int high){
-        if(root==NULL) return;
-        while(root->left != NULL){
-            if(root->left->val < low) root->left = root->left->right;
-            else if(root->left->val > high) root->left = root->left->left;
-            else break;
-        }
-         while(root->right != NULL){
-            if(root->right->val > high) root->right = root->right->left;
-            else if(root->right->val < low) root->right = root->right->right;
-            else break;
-        }
-        // root = [1,0,2] -> dry run to understand why we need recursion
-        Trim(root->left,low,high);
-        Trim(root->right,low,high);
-    }
-    TreeNode* trimBST(TreeNode* root, int low, int high) {
-        TreeNode*dummy=new TreeNode(INT_MAX);
-        dummy->left=root;
-        Trim(dummy,low,high);
-        return dummy->left;
+        
     }
 };
