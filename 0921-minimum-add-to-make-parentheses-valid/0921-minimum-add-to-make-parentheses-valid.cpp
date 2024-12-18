9bd4085589a25a3bff27f280e70a0d72ffa33803
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-12-18 11:16:57 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (11.46%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -3,33 +3,36 @@ public:
     int minAddToMakeValid(string s) {
         
         
-        stack<char> st;
         int ans=0;
+        stack<char> st;
         
         for(int i=0;i<s.length();i++)
         {
+            
             if(s.at(i)=='(')
             {
                 st.push(s.at(i));
             }else{
                 
-                if(!st.empty())
+                if(st.empty())
                 {
-                    st.pop();
-                }else{
-                    ans++;
-                }
-                
-                
+                   ans+=1; 
+                    continue;
+                }    
+                    
+                st.pop();
                 
             }
             
+            
+            
         }
+        
+        cout<<"the ans is "<<ans<<endl;
         
         ans+=st.size();
         
+        
         return ans;
-        
-        
     }
 };
