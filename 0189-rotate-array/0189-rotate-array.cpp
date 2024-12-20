b0fa58fc5a0a7e5991d7a5cc8001b664c1fbb413
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-12-20 17:52:33 +0530

    Time: 0 ms (100.00%), Space: 30.5 MB (7.06%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,49 +1,22 @@
-
-
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         
-       /* int a = k%nums.size();
+        int n = nums.size();
         
-        while(a!=0)
-        {
-            int num = nums.back();
-            nums.pop_back();
-            nums.insert(nums.begin(),num);
-            a--;
-            
-        }*/
+        k = k%n;
         
-        k=k%nums.size();
-        
-        
-        /*vector<int>:: iterator it;
-        
-        it=nums.end()-(k);
-       
-        
-        
-        
-        vector<int> v(it,nums.end());
-        
-        nums.erase(nums.end()-k,nums.end());
-        
-        vector<int> merged = v;
-       merged.insert(merged.end(), nums.begin(), nums.end());
-        
-        nums=merged;*/
-        
-        
-        vector<int> v(nums.size());
+        vector<int> ans(nums.size());
         
         for(int i=0;i<nums.size();i++)
         {
-            v[(i+k)%nums.size()]=nums[i];
+              int h = (i+k)%n;
+            
+             ans[h]=nums[i];
         }
-           
         
-        nums=v;
+        
+       nums=ans;
         
         
     }
