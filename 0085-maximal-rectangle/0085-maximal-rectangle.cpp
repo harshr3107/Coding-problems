ed17da9470b88e66c95a8e310f2840a8f1088beb
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-12-23 13:14:38 +0530

    Time: 16 ms (26.99%), Space: 21.4 MB (19.22%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -4,117 +4,8 @@ public:
     
     
     
-    void getallnextsmallest(vector<int> v,stack<int> st,vector<int>& nextsmallest)
+    void display(vector<int>& v)
     {
-        
-        for(int i=v.size()-2;i>=0;i--)
-        {
-            while(st.top()!=-1 && v[st.top()]>=v[i])
-            {
-                st.pop();
-            }
-            
-            nextsmallest[i]=st.top();
-            st.push(i);
-            
-        }
-        
-        
-        
-        
-    }
-    
-    void getallprevsmallest(vector<int> v,stack<int> st,vector<int>& prevsmallest)
-    {
-        
-        for(int i=1;i<v.size();i++)
-        {
-            while(st.top()!=-1 && v[st.top()]>=v[i])
-            {
-                st.pop();
-            }
-            
-            prevsmallest[i]=st.top();
-            st.push(i);
-            
-            
-        }
-        
-        
-        
-    }
-    
-    
-    
-    
-    
-    
-    int getminarea(vector<int> v)
-    {
-        
-        int area = 0;
-        
-        stack<int> st1;
-        vector<int> nextsmallest(v.size());
-        nextsmallest[v.size()-1]=-1;
-        st1.push(-1);
-        st1.push(v.size()-1);
-        
-        
-        stack<int> st2;
-        vector<int> prevsmallest(v.size());
-        st2.push(-1);
-        st2.push(0);
-        prevsmallest[0]=-1;
-        
-        getallnextsmallest(v,st1,nextsmallest);
-        getallprevsmallest(v,st2,prevsmallest);
-        
-
-        
-        
-        //display(nextsmallest);
-        //display(prevsmallest);
-        
-        
-        
-        
-        
-       for(int i=0;i<v.size();i++)
-        {
-           if(v[i]==0)
-           {
-               
-               continue;
-           }
-           
-            int prev = prevsmallest[i];
-            int next = nextsmallest[i];
-            
-            if(next==-1)
-            {
-                next=v.size();
-            }    
-            
-            area = max(area, v[i]*(next-prev-1));
-            
-        }
-        
-        //cout<<"area is "<<area<<endl;
-        
-        return area;
-        
-    }
-    
-    
-    
-    
-    
-    
-    
-  /*void display(vector<int> v)
-    {
-         
         for(int i=0;i<v.size();i++)
         {
             cout<<v[i]<<" ";
@@ -123,73 +14,155 @@ public:
         cout<<endl;
     }
     
-    */
+    
+    
+    
+    
+    void getnextsmallest(vector<int>& height,vector<int>& nextsmallest)
+    {
+       // cout<<endl;
+        stack<int> st;
+        st.push(-1);
+        
+        int i=height.size()-1;
+        
+        st.push(i);
+        
+        nextsmallest[i]=-1;
+        
+        while(i>=0)
+        {
+            
+            while(st.top()!=-1 && height[st.top()]>=height[i])
+            {
+                st.pop();
+            }
+            
+            nextsmallest[i]=st.top();
+            
+            st.push(i);
+            
+            i--;
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    void getprevsmallest(vector<int>& height,vector<int>& prevsmallest)
+    {
+        //cout<<endl;
+        stack<int> st;
+        st.push(-1);
+        
+        int i=0;
+        
+        st.push(i);
+        
+        prevsmallest[i]=-1;
+        
+        while(i<height.size())
+        {
+            
+            while(st.top()!=-1 && height[st.top()]>=height[i])
+            {
+                st.pop();
+            }
+            
+            prevsmallest[i]=st.top();
+            
+            st.push(i);
+            
+            i++;
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    int getmaxarea(vector<int>& height)
+    {
+        
+        vector<int> nextsmallest(height.size());
+        getnextsmallest(height,nextsmallest);
+        vector<int> prevsmallest(height.size());
+        getprevsmallest(height,prevsmallest);
+        
+       
+        int ans=INT_MIN;
+        
+        for(int i=0;i<height.size();i++)
+        {
+            
+            if(height[i]==0)
+            {
+                continue;
+            }
+            
+            if(nextsmallest[i]==-1)
+            {
+                nextsmallest[i]=height.size();
+            }    
+            
+            ans = max(ans,height[i]*(nextsmallest[i]-prevsmallest[i]-1));
+            
+        }
+        
+        
+        
+        
+        
+        return ans;
+        
+        
+    }
+    
+    
+    
     
     int maximalRectangle(vector<vector<char>>& matrix) {
         
+        vector<int> height(matrix[0].size());
         
-        int area=INT_MIN;
+        int ans=0;
         
-        vector<int> v;
-        
-        int row=0;
-        
-        for(int i=0;i<matrix[0].size();i++)
+        for(int i=0;i<matrix.size();i++)
         {
-            v.push_back(matrix[row][i]-'0');
-        }
-        
-        //display(v);
-        area = getminarea(v);
-        
-        
-        //cout<<"initial area is "<<area<<endl;
-        
-        
-      
-        
-        
-        for(int i=1;i<matrix.size();i++)
-        {
+            
             for(int j=0;j<matrix[i].size();j++)
             {
-                int num = v[j]+matrix[i][j]-'0';
                 
-                if(matrix[i][j]=='0')
+                if(matrix[i][j]=='1')
                 {
-                    
-                     v[j]=0;
-                    
+                    height[j]+=1;
                 }else{
-                    v[j]=num;
-                }  
+                    
+                    height[j]=0;
+                }
                 
                 
-                
-                
-                //cout<<v[j]-'0'<<" "<<matrix[i][j]-'0'<<endl;
-                //cout<<"value of num is "<<num<<endl;
-                 
-                
-                
-                
-             }
+            }
             
-           // display(v);
-          //cout<<endl;
+            //display(height);
+            ans = max(ans,getmaxarea(height));
             
-           area = max(area,getminarea(v));   
+            
             
         }
         
         
         
         
-       
-        
-        
-        return area;
-        
-        
+        return ans;
     }
 };
