8872e39bb59ab8d09e5d3870c04f5be8dbc085ba
Harsh Rathi <142046984+harshr3107@users.noreply.github.com>
2024-12-26 10:27:53 +0530

    Time: 648 ms (5.07%), Space: 378.4 MB (5.21%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,36 +1,75 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
+    
+    
+    int getminimum(string word1,string word2,int ind1,int ind2,vector<vector<int>>& dp)
+    {
         
-        vector<vector<int>> dp(word1.length()+1,vector<int>(word2.length()+1,0));
-        
-        
-        for(int i=1;i<=word1.length();i++)
+        if(ind1==word1.length() && ind2==word2.length())
         {
-            for(int j=1;j<=word2.length();j++)
-            {
-                if(word1.at(i-1)==word2.at(j-1))
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                    continue;
-                }
-                
-                int left = dp[i-1][j];
-                int right = dp[i][j-1];
-                
-                dp[i][j]=max(left,right);
-                
-                
-                
-            }
-            
+            return 0;
+        }
+        
+        
+        if(ind1>=word1.length())
+        {
+            return word2.length()-ind2;
+        }
+        
+        
+         if(ind2>=word2.length())
+         {
+             return word1.length()-ind1;
+         }
+        
+        
+        if(dp[ind1][ind2]!=-1)
+        {
+            return dp[ind1][ind2];
         }
         
         
         
-       int h = dp[word1.length()][word2.length()];
         
-        return (word1.length()-h)+(word2.length()-h);
+        int no_opp=1e9+7;
+        int d1=1e9+7;
+        int d2=1e9+7;
+        
+        
+        if(word1.at(ind1)==word2.at(ind2))
+        {
+            no_opp = getminimum(word1,word2,ind1+1,ind2+1,dp);
+            dp[ind1][ind2]=no_opp;
+            return dp[ind1][ind2];
+        }else{
+            
+            d1 = 1+getminimum(word1,word2,ind1+1,ind2,dp);
+            d2 = 1+getminimum(word1,word2,ind1,ind2+1,dp);
+            
+        }
+        
+        
+        dp[ind1][ind2] = min(no_opp,min(d1,d2));
+        return dp[ind1][ind2];
+        
+    }
+    
+    
+    
+    
+    
+    
+    int minDistance(string word1, string word2) {
+        
+        int ind1=0;
+        int ind2=0;
+        
+        vector<vector<int>> dp(word1.length(),vector<int>(word2.length(),-1));
+        
+        
+        
+        return getminimum(word1,word2,ind1,ind2,dp);
+        
         
     }
 };
